14729f7269f6f09e3282baa5e79e2b096e886ef1
Robin5605 <robinjefferson123@gmail.com>
2023-02-04 19:51:42 -0600

    Support dark mode in posts

--- pages/[post].tsx
+++ pages/[post].tsx
@@ -37,13 +37,13 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 
 export default function PostPage({ title, description, content }: BlogPost) {
     return (
-        <div className="w-screen min-h-screen bg-nord-500">
+        <div className="w-screen min-h-screen bg-nord-500 dark:bg-nord-200">
             <Navbar selected={null} />
             <div className="mx-auto py-16 px-8 w-fit">
-                <div className="text-4xl font-bold">{ title }</div>
-                <div className="text-xl font-medium italic text-gray-500">{ description }</div>
+                <div className="text-4xl font-bold dark:text-nord-700">{ title }</div>
+                <div className="text-xl font-medium italic text-gray-500 dark:text-nord-500">{ description }</div>
                 <div 
-                    className="mt-4 prose lg:prose-xl" 
+                    className="mt-4 prose lg:prose-xl dark:prose-invert" 
                     dangerouslySetInnerHTML={{__html: marked.parse(content ?? "")}}
                 >
 
