c9191bd7f8827f485838045b76b02e661878e252
Robin5605 <robinjefferson123@gmail.com>
2023-03-17 21:14:38 -0500

    Log AWS Region from environment variables

--- s3.ts
+++ s3.ts
@@ -4,11 +4,14 @@ import { BlogPost } from "./models/blog";
 
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_REGION = process.env.AWS_REGION;
+
 const REGION = "us-east-1";
 const BUCKET = "robinj-blog-articles";
 
 console.log("AWS_ACCESS_KEY_ID", typeof AWS_ACCESS_KEY_ID);
 console.log("AWS_SECRET_ACCESS_KEY", typeof AWS_SECRET_ACCESS_KEY);
+console.log("AWS_REGION", AWS_REGION);
 
 const s3Client = new S3Client({ 
     region: REGION,
